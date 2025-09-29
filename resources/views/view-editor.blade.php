b17e15f9eaa1da513a7fa036a0fab8b70cae30d7
campaigncenter <73285201+campaigncenter@users.noreply.github.com>
2025-09-29 15:53:53 +0200

    Filament 4 fixes

--- resources/views/view-editor.blade.php
+++ resources/views/view-editor.blade.php
@@ -1,12 +1,12 @@
 <x-filament-panels::page>
-    <x-filament-panels::form wire:submit="save">
+    <form wire:submit="save">
         {{ $this->form }}
 
-        <x-filament-panels::form.actions
+        <x-filament::actions
             :actions="$this->getCachedFormActions()"
             :full-width="$this->hasFullWidthFormActions()"
         />
-    </x-filament-panels::form>
+    </form>
 
     <x-filament-panels::page.unsaved-data-changes-alert />
 </x-filament-panels::page>
